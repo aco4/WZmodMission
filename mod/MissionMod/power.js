ca69ab71ce3ea600b56d56202f700586bb327fc5
aco4 <anthonythegu@gmail.com>
2026-06-19 23:39:48 -0400

    Always use no-base starting power

--- mod/MissionMod/power.js
+++ mod/MissionMod/power.js
@@ -4,18 +4,7 @@ function power_eventStartLevel()
 {
 	for (let player = 0; player < maxPlayers; player++)
 	{
-		if (baseType === CAMP_CLEAN)
-		{
-			setPower(1300, player);
-		}
-		else if (baseType === CAMP_BASE)
-		{
-			setPower(2500, player);
-		}
-		else // CAMP_WALLS
-		{
-			setPower(2500, player);
-		}
+		setPower(1300, player);
 
 		if (powerType === 0)
 		{
